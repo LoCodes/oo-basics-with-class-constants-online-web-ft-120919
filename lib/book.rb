5f2a0614e062bd4da2eef57c6009980cf322d6c1
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-20 05:28:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,6 +12,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
   
+   # create the writer for genre and add the logic for the class constant
+  
   def genr=(genre)
     @genre = genre 
     GENRES << genre
