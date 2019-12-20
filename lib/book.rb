ef03d2b574f6573c720a1aad8816f5122c9ba40e
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-20 05:30:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,7 +14,7 @@ class Book
   
    # create the writer for genre and add the logic for the class constant
   
-  def genre(genre)
+  def genre=(genre)
     @genre = genre 
     GENRES << genre
   end
